f9a9694dade81e46bb33a74419373078ac338e42
Javier Acevedo <javieracevedo1987@gmail.com>
2021-10-22 18:08:29 +0200

    feat: Login componetization

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,24 +1,31 @@
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Switch, Route, useHistory } from 'react-router-dom'
 import { Menu } from '../shared/layout/Menu'
 import { Login } from '../user/containers/Login'
 import { GroupDetail } from '../group/containers/GroupDetail'
 import { Groups } from '../group/containers/Groups'
 
 export const Router = () => {
-  const isAuth = true
+  const history = useHistory()
+  const isAuth = false
+
+  useEffect(() => {
+    if (!isAuth) {
+      history.push('/login')
+    }
+  }, [history, isAuth])
 
   return isAuth ? (
-    <Menu>
-      <Switch>
-        <Route exact path="/">
-          <Redirect to="/groups" />
-        </Route>
-        <Route exact path="/groups" component={Groups} />
-        <Route path="/groups/:id" component={GroupDetail} />
-        <Route path="/login" component={Login} />
-        <Route path="*" component={() => <h1>NOT FOUND!!!</h1>} />
-      </Switch>
-    </Menu>
+    <Switch>
+      <Menu>
+        <Switch>
+          <Route exact path="/groups" component={Groups} />
+          <Route path="/groups/:id" component={GroupDetail} />
+          <Route path="*" component={() => <h1>NOT FOUND!!!</h1>} />
+        </Switch>
+      </Menu>
+      <Route path="/login" component={Login} />
+    </Switch>
   ) : (
     <Login />
   )
--- src/user/containers/Login.tsx
+++ src/user/containers/Login.tsx
@@ -1,7 +1,51 @@
-import { LoginForm } from '../components/LoginForm'
+import React from 'react'
+import styles from './Login.module.css'
 
-export const Login: React.FC = () => (
-  <div className="login">
-    <LoginForm />
-  </div>
-)
+export const Login: React.FC = () => {
+  const [username, setUsername] = React.useState<string>('')
+  const [password, setPassword] = React.useState<string>('')
+
+  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputUsername = event.target.value
+    setUsername(inputUsername)
+  }
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputPassword = event.target.value
+    setPassword(inputPassword)
+  }
+
+  const handleSubmit = (form: React.FormEvent) => {
+    form.preventDefault()
+    console.log('FORM LOGIN !!!', { username, password })
+  }
+
+  return (
+    <div className={styles.loginForm}>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            name="user"
+            value={username}
+            placeholder="Username"
+            onChange={handleUsername}
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Password"
+            onChange={handlePassword}
+          />
+        </div>
+
+        <div className="form-group">
+          <button className="button">LOGIN!</button>
+        </div>
+      </form>
+    </div>
+  )
+}
