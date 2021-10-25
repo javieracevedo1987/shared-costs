2ad0b1668b9c7181110b6704c7538ceb54cdfaf1
Javier Acevedo <javieracevedo1987@gmail.com>
2021-10-25 11:22:48 +0200

    feat: Route group detail

--- src/modules/group/components/GroupCard/GroupCard.tsx
+++ src/modules/group/components/GroupCard/GroupCard.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import styles from './GroupCard.module.css'
 
 interface GroupCardProps {
   group: IGroup
-  handleCard: Function
 }
 
-export const GroupCard: React.FC<GroupCardProps> = ({ group, handleCard }) => {
+export const GroupCard: React.FC<GroupCardProps> = ({ group }) => {
+  const history = useHistory()
+
+  const handleCard = () => {
+    history.push(`/groups/${group.id}`)
+  }
+
   return (
-    <article onClick={() => handleCard(group)} className={styles.groupCard}>
+    <article onClick={handleCard} className={styles.groupCard}>
       <div className={styles.header}>{group.name}</div>
       <div className={styles.body}>1 Member</div>
     </article>
--- src/modules/group/components/GroupList/GroupList.tsx
+++ src/modules/group/components/GroupList/GroupList.tsx
@@ -7,14 +7,10 @@ interface GroupListProps {
 }
 
 export const GroupList: React.FC<GroupListProps> = ({ groups }) => {
-  const handleCard = (group: IGroup) => {
-    console.log('GROUP', group)
-  }
-
   return (
     <section className={styles.groupList}>
       {groups.map((group: IGroup) => (
-        <GroupCard key={group.id} group={group} handleCard={handleCard} />
+        <GroupCard key={group.id} group={group} />
       ))}
     </section>
   )
